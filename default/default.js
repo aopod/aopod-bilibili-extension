ed4a8fd29169c07cf663ee4a0073aa01b6633785
aopod <aopodcom@gmail.com>
2017-04-11 23:39:01 +0800

    顶部添加我的关注

--- default/default.js
+++ default/default.js
@@ -1,3 +1,8 @@
 (function() {
-	console.log('Hello World');
+	// 添加我的关注
+	var top_ul = document.body.querySelector('.z_top .z_top_nav ul');
+	var my_follow = document.createElement('li');
+	my_follow.setAttribute('class', 'aopod_my_follow');
+	my_follow.innerHTML = '<a class="i-link" href="//space.bilibili.com/#!/fans/follow">我的关注</a>';
+	top_ul.appendChild(my_follow);
 })();
